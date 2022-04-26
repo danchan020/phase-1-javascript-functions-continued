e9d2225fa7e13cf87c4a19a254cce152cb07f62a
Daniel <danchan020@gmail.com>
2022-04-26 03:23:15 -0700

    commit tree 241383005ae03ce00970d1441b02ef6759f7401a

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return(`This Saturday, I want to ${activity}!`)}
+
+function mondayWork(remote="go to the office"){
+    return (`This Monday, I will ${remote}.`)
+}
+function wrapAdjective(a="special"){return "You are *a hard worker*!"}
